fe5d1a89c8529a2cb3f7803b2fceba20353ff55d
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2014-11-29 21:24:26 -0500

    This.

---
+++ mud/main/Commandset.java
@@ -0,0 +1,90 @@
+package main;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.reflect.Method;
+
+/** Commands.
+ @author Neil */
+
+public class Commandset {
+
+	private final String name;
+	private final Map<String, Method> commands = new HashMap<String, Method>();
+
+	public Commandset(String name) {
+		Class<?> c = this.getClass();
+		this.name = name;
+		try {
+			//Method m = c.getMethod("say", String.class);
+			Method m = c.getDeclaredMethod("say", String.class);
+			commands.put("say", m);
+			//commands.put("say", Commandset.class.getMethod("say", Connection.class, String.class));
+			//commands.put("say", Commands.class.getMethod("say", Connection.class, String.class));
+		} catch (NoSuchMethodException e) {
+			System.err.print(this + ": " + e + "!\n");
+		}
+	}
+
+	/** This parses the string and runs it.
+	 @param cmd
+	 A command to parse.
+	 @return A method if the string was an actual command. */
+	public void interpret(final Connection c, final String cmd) {
+		System.err.print("Command::interpret: " + cmd + ".\n");
+		Method run = commands.get(cmd);
+		if(run == null) {
+			System.out.print("Huh? " + cmd + "\n");
+		} else {
+			try {
+				run.invoke(c, cmd);
+			} catch(Exception e) {
+				System.err.print("Can't do that: " + e + ".\n");
+			}
+		}
+	}
+
+	public String toString() {
+		return "CommandSet " + name;
+	}
+
+	static void say(String l) {
+		System.out.print("Worked " + l + ".\n");
+	}
+/*	static void say(final Connection c, final String line) {
+		System.out.print(c + ": " + line + "\n");
+	}*/
+
+//	private static final Map<String, Method> commands = new HashMap<String, Method>();
+
+/*	static {
+		try {
+			commands.put("say", Commands.class.getMethod("say", Connection.class, String.class));
+		} catch (NoSuchMethodException e) {
+			System.err.print("Building command table: " + e + "!\n");
+		}
+	}
+
+	static void say(final Connection c, final String line) {
+		System.out.print(c + ": " + line + "\n");
+	}*/
+
+	/** This parses the string and runs it.
+	 @param cmd
+		A command to parse.
+	 @return A method if the string was an actual command. */
+/*	public static void interpret(final Connection c, final String cmd) {
+		System.err.print("Command::interpret: " + cmd + ".\n");
+		Method run = commands.get(cmd);
+		if(run == null) {
+			System.out.print("Huh? " + cmd + "\n");
+		} else {
+			try {
+				run.invoke(c, cmd);
+			} catch(Exception e) {
+				System.err.print("Can't do that: " + e + ".\n");
+			}
+		}
+	}*/
+
+}
